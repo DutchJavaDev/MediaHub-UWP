48c165435eb4243ed84e25a60ed31486489e3773
Boris <Borismulder@outlook.com>
2021-02-04 20:04:15 +0100

    Helper class, might delete this tho

---
+++ MediaHub UWP/Helper.cs
@@ -0,0 +1,15 @@
+﻿
+using TMDbLib.Client;
+
+namespace MediaHub_UWP
+{
+    public static class Helper
+    {
+        private static readonly string API_KEY = "key here";
+
+        public static TMDbClient CreateClient()
+        {
+            return new TMDbClient(API_KEY);
+        }
+    }
+}
