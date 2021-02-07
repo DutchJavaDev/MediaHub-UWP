f21c559eb66c5bb5b88a1e6050254c7ae40c5bda
Boris <Borismulder@outlook.com>
2021-02-08 00:20:50 +0100

    Splitted widget into two classes but they both take the same response from the api

---
+++ MediaHub UWP/Controls/ShowWidget.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TMDbLib.Objects.Search;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MediaHub_UWP.Controls
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShowWidget : UserControl
+    {
+        private readonly SearchTv Model;
+
+        private string PostPath { get; set; }
+
+        public ShowWidget(SearchMovieTvBase tv)
+        {
+            InitializeComponent();
+            Model = (SearchTv) tv;
+            PostPath = $@"https://image.tmdb.org/t/p/original/{Model.BackdropPath}";
+        }
+    }
+}
