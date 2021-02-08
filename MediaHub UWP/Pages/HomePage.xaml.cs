1f39c4c9af7f93b01bf0f467ee29cb1174fa4318
Boris <Borismulder@outlook.com>
2021-02-08 22:56:34 +0100

    Basis for the homepage, movies/shows are now being loaden and displayed like i wanted.

--- MediaHub UWP/Pages/HomePage.xaml.cs
+++ MediaHub UWP/Pages/HomePage.xaml.cs
@@ -1,12 +1,10 @@
 ﻿using MediaHub_UWP.Controls;
-using TMDbLib.Client;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Controls;
 using System.Collections.ObjectModel;
 using TMDbLib.Objects.Search;
 using System.Linq;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -30,24 +28,56 @@ namespace MediaHub_UWP.Pages
 
     public sealed partial class HomePage : Page
     {
-        private readonly ObservableCollection<MovieWidget> Movies = new ObservableCollection<MovieWidget>();
-        private readonly ObservableCollection<ShowWidget> Tv = new ObservableCollection<ShowWidget>();
-        private readonly ObservableCollection<MovieWidget> MoviesSugestions = new ObservableCollection<MovieWidget>();
-        private readonly ObservableCollection<ShowWidget> TvSugestions = new ObservableCollection<ShowWidget>();
+        private readonly ObservableCollection<MovieWidget> ObservableCollectionForMovies = new ObservableCollection<MovieWidget>();
+        private readonly ObservableCollection<ShowWidget> ObservableCollectionForTvShows = new ObservableCollection<ShowWidget>();
+        private readonly ObservableCollection<MovieWidget> ObservableCollectionForMoviesSugestions = new ObservableCollection<MovieWidget>();
+        private readonly ObservableCollection<ShowWidget> ObservableCollectionForTvSugestions = new ObservableCollection<ShowWidget>();
+
+        private readonly ListViewWidget PopularMovies = new ListViewWidget
+        {
+            HeaderText = "Popular Movies"
+        };
+
+        private readonly ListViewWidget MoviesSuggestions = new ListViewWidget
+        {
+            HeaderText = "Movies Suggestions"
+        };
+
+        private readonly ListViewWidget PopularShows = new ListViewWidget 
+        {
+            HeaderText = "Popular Shows"
+        };
+
+        private readonly ListViewWidget ShowsSuggestions = new ListViewWidget 
+        {
+            HeaderText = "Shows Suggestion"
+        };
 
         public HomePage()
         {
             InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Required;
+
+            // Bind the collections to the lists
+            PopularMovies.SetItemSource(ObservableCollectionForMovies);
+            MoviesSuggestions.SetItemSource(ObservableCollectionForMoviesSugestions);
+            PopularShows.SetItemSource(ObservableCollectionForTvShows);
+            ShowsSuggestions.SetItemSource(ObservableCollectionForTvSugestions);
+
+            // Add them to the stackpanel
+            PageContents.Children.Add(PopularMovies);
+            PageContents.Children.Add(MoviesSuggestions);
+            PageContents.Children.Add(PopularShows);
+            PageContents.Children.Add(ShowsSuggestions);
         }
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             await LoadMovieCollection(LoadAction.Movie);
+            await LoadTvCollection(LoadAction.Tv);
             await LoadMovieCollection(LoadAction.MovieSuggestion);
             await LoadTvCollection(LoadAction.TvSuggestion);
         }
-
 
         private async Task LoadMovieCollection(LoadAction action)
         {
@@ -60,7 +90,7 @@ namespace MediaHub_UWP.Pages
                             var collection = (await client.GetMoviePopularListAsync(page: 1)).Results.Select(i => (SearchMovieTvBase)i).ToList();
 
                             foreach (var item in collection)
-                                Movies.Add(new MovieWidget(item));
+                                ObservableCollectionForMovies.Add(new MovieWidget(item));
                         }
                         break;
 
@@ -70,7 +100,7 @@ namespace MediaHub_UWP.Pages
                             var collection = (await client.GetMovieRecommendationsAsync(293863, page: 1)).Results.Select(i => (SearchMovieTvBase)i).ToList();
 
                             foreach (var item in collection)
-                                MoviesSugestions.Add(new MovieWidget(item));
+                                ObservableCollectionForMoviesSugestions.Add(new MovieWidget(item));
                         }
                         break;
 
@@ -91,7 +121,7 @@ namespace MediaHub_UWP.Pages
                             var collection = (await client.GetTvShowPopularAsync(page: 1)).Results.Select(i => (SearchMovieTvBase)i).ToList();
 
                             foreach (var item in collection)
-                                Tv.Add(new ShowWidget(item));
+                                ObservableCollectionForTvShows.Add(new ShowWidget(item));
                         }
                         break;
 
@@ -101,7 +131,7 @@ namespace MediaHub_UWP.Pages
                             var collection = (await client.GetTvShowRecommendationsAsync(62017,page: 1)).Results.Select(i => (SearchMovieTvBase)i).ToList();
 
                             foreach (var item in collection)
-                                TvSugestions.Add(new ShowWidget(item));
+                                ObservableCollectionForTvSugestions.Add(new ShowWidget(item));
                         }
                         break;
 
@@ -113,7 +143,6 @@ namespace MediaHub_UWP.Pages
 
         private void Page_Unloaded(object sender, RoutedEventArgs e)
         {
-
         }
     }
 }
