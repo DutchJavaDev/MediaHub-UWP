64bb5af151555c35b5c2cc1fc788a766779f0140
Boris <Borismulder@outlook.com>
2021-02-08 22:44:37 +0100

    Fixed issues with my custom controls not loading/showing, Started using binding and ObservableCollection.

---
+++ MediaHub UWP/Controls/ListViewWidget.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MediaHub_UWP.Controls
+{
+    public sealed partial class ListViewWidget : UserControl
+    {
+        public readonly Orientation OrientationView = Orientation.Horizontal;
+        public string HeaderText { get; set; } = string.Empty;
+
+        public ListViewWidget()
+        {
+            InitializeComponent();
+        }
+
+        public void SetItemSource<T>(ObservableCollection<T> collection)
+        {
+            View.SetBinding(ItemsControl.ItemsSourceProperty, new Binding { Source = collection });
+        }
+
+        private void ItemsStackPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            // I can only reach this like this -.-
+            var stackPanel = (ItemsStackPanel)sender;
+            stackPanel.Orientation = OrientationView;
+        }
+    }
+}
