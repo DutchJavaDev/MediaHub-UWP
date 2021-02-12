8607e465bbf63855d20041ad60f12651c504032f
Boris <Borismulder@outlook.com>
2021-02-12 17:35:33 +0100

    Added unit test

---
+++ MediaHub UWP UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MediaHub UWP UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MediaHub UWP UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ MediaHub UWP UnitTest/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MediaHub_UWP_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
